23f5d98abd95dea51b838fca9d6127960fe790d6
Colin Read (coread) <coread@cisco.com>
2014-06-05 10:25:31 +0100

    Add confirm to post it removal

--- static/kanban.js
+++ static/kanban.js
@@ -10,11 +10,14 @@ $('.kanban_col').mousedown(function(e) {
 	new_postit.attr("id", "postit_" + (NUMBER_ISSUES+1))
 	new_postit.select('.close_icon').click(function(e){
 		console.log("closing")
-		/*elements_under_point = $(document.elementFromPoint(e.pageX, e.pageY))
+		elements_under_point = $(document.elementFromPoint(e.pageX, e.pageY))
 		if (!elements_under_point.is('.close_icon')) {
 			console.log("not close icon", elements_under_point)
 			return
-		}*/
+		}
+        if (!confirm("this will remove the post it")) {
+            return
+        }
 		new_postit.remove()
 		console.log("finished")
 	})
